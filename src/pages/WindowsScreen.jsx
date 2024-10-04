c757229fc48a38be3b1387ca3b42f9e2f57bc31c
ErikPradoDev <erikpcdev@gmail.com>
2024-10-03 22:02:32 -0300

    General changes

--- src/pages/WindowsScreen.jsx
+++ src/pages/WindowsScreen.jsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import NavBar from '../components/Navbar';
+import NavBar from '../components/NavBar.jsx';
 import DesktopIcons from '../components/DesktopIcons';
-
-
 import '../styles/components/windows.sass';
 import '../styles/components/windows.css';
 
